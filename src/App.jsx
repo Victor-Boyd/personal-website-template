1dff3bd9b973dfee8e2a60d747a5328044c7e648
Victor <victor.w.boyd@gmail.com>
2023-11-02 15:05:18 -0500

    completed resume section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from './components/navbar.jsx'
 import Hero from './components/Hero.jsx'
 import About from './components/About.jsx'
+import Resume from './components/Resume.jsx'
 import './App.css'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <Resume />
     </>
   )
 }
---
+++ src/components/Resume.jsx
@@ -0,0 +1,13 @@
+import './Resume.css'
+
+function Resume() {
+    return (
+        <div className="resume-container w-full h-auto flex justify-center">
+            <span className="resume-span bg-black justify-center  max-w-2xl  w-3/4 grid place-items-center text-white text-3xl font-black">
+                your resume here!
+            </span>
+        </div>
+    )
+}
+
+export default Resume
